64a9940073248ba41d394282316cb6493863933a
PRANAY <kumarpusp2000@gmail.com>
2023-06-03 17:35:46 +0530

    initial commit

---
+++ covid_tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Statewise from './components/StateWiseData/statewise';
+
+function App() {
+  return (
+    <>
+     <Statewise />
+    </>
+  );
+}
+
+export default App;
